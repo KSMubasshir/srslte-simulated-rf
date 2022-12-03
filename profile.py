ba5ac1f9fa8882ca5fbcad1cd8cfc0a509a0be15
ksmubasshir <ksmubasshir@gmail.com>
2022-12-02 19:44:39 -0500

    epc2 added

--- profile.py
+++ profile.py
@@ -75,35 +75,46 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Add EPC node
-epc = request.RawPC("epc")
+epc = request.RawPC("epc_real")
 epc.hardware_type = GLOBALS.HWTYPE
 epc.disk_image = GLOBALS.SRSLTE_IMG
 iface1 = epc.addInterface("eth1")
 iface1.addAddress(rspec.IPv4Address("10.10.1.1", "255.255.255.0"))
+
+# Add Malicious EPC node
+epc2 = request.RawPC("epc_fake")
+epc2.hardware_type = GLOBALS.HWTYPE
+epc2.disk_image = GLOBALS.SRSLTE_IMG
+iface2 = epc2.addInterface("eth1")
+iface2.addAddress(rspec.IPv4Address("10.10.1.2", "255.255.255.0"))
+
 # Add eNB node
-enb1 = request.RawPC("enb1")
+enb1 = request.RawPC("enb_real")
 enb1.hardware_type = GLOBALS.HWTYPE
 enb1.disk_image = GLOBALS.SRSLTE_IMG
-iface2 = enb1.addInterface("eth1")
-iface2.addAddress(rspec.IPv4Address("10.10.1.2", "255.255.255.0"))
-# Add eNB node
-enb2 = request.RawPC("enb2")
+iface3 = enb1.addInterface("eth1")
+iface3.addAddress(rspec.IPv4Address("10.10.1.3", "255.255.255.0"))
+
+# Add Malicious eNB node
+enb2 = request.RawPC("enb_fake")
 enb2.hardware_type = GLOBALS.HWTYPE
 enb2.disk_image = GLOBALS.SRSLTE_IMG
-iface3 = enb2.addInterface("eth1")
-iface3.addAddress(rspec.IPv4Address("10.10.1.3", "255.255.255.0"))
+iface4 = enb2.addInterface("eth1")
+iface4.addAddress(rspec.IPv4Address("10.10.1.4", "255.255.255.0"))
+
 # Add UE node
 rue1 = request.RawPC("rue1")
 rue1.hardware_type = GLOBALS.HWTYPE
 rue1.disk_image = GLOBALS.SRSLTE_IMG
-iface4 = rue1.addInterface("eth1")
-iface4.addAddress(rspec.IPv4Address("10.10.1.4", "255.255.255.0"))
+iface5 = rue1.addInterface("eth1")
+iface5.addAddress(rspec.IPv4Address("10.10.1.5", "255.255.255.0"))
 
 link = request.LAN("lan")
 link.addInterface(iface1)
 link.addInterface(iface2)
 link.addInterface(iface3)
 link.addInterface(iface4)
+link.addInterface(iface5)
 link.link_multiplexing = True
 link.vlan_tagging = True
 link.best_effort = True
